5628f0ea45843a94d126edd6b6c435ad20314c18
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-11-08 00:44:09 -0300

    areglos

--- Assets/ProyectoFinal/Player/scripsPayer/PlayerMovement.cs
+++ Assets/ProyectoFinal/Player/scripsPayer/PlayerMovement.cs
@@ -1,30 +1,23 @@
-using UnityEngine;
-
+using UnityEngine; 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 10f;
     private Rigidbody rb;
-
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-       // rb.freezeRotation = true;
+        // rb.freezeRotation = true; 
     }
-
     void Update()
     {
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoVertical = Input.GetAxis("Vertical");
-
         Vector3 velocity = Vector3.zero;
-
         if (movimientoHorizontal != 0 || movimientoVertical != 0)
         {
             Vector3 direccion = (transform.forward * movimientoVertical + transform.right * movimientoHorizontal).normalized;
             velocity = direccion * speed;
         }
-
-        velocity.y = rb.linearVelocity.y;
-        rb.linearVelocity = velocity;
-    }
+        velocity.y = rb.linearVelocity.y; rb.linearVelocity = velocity;
+    } 
 }
